c1340c6b17216512a99f5f5ee7e1f0e0f267ba0c
jasonl0215 <60702463+jasonl0215@users.noreply.github.com>
2020-02-12 09:46:48 -0500

    Conditions.py

---
+++ conditions.py
@@ -0,0 +1,9 @@
+#This is a comment
+temp = input("Please enter the current temperature: ")
+temp = int(temp)
+if temp >= 70:
+    print ("No jacket required")
+else:
+    print("Wear a jacket")
+
+    
